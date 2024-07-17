bb3af7355be714d479117365f2d55717c3c93ab8
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-07-17 21:38:10 +0700

    Update aut.sh

--- aut.sh
+++ aut.sh
@@ -3,9 +3,9 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
 apt-get install -f -y
 wget https://storage.googleapis.com/chrome-for-testing-public/126.0.6478.182/linux64/chromedriver-linux64.zip
-
 unzip chromedriver-linux64.zip
-
+cd chromedriver-linux64
+sudo cp -r * ..
 sudo mv chromedriver /usr/bin/chromedriver
 sudo chown root:root /usr/bin/chromedriver
 sudo chmod +x /usr/bin/chromedriver
