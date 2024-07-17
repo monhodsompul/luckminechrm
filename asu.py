01ce9d8cd5753cc5bbf329c2e54ea14f8e0adb2a
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-07-18 05:06:57 +0700

    Update asu.py

--- asu.py
+++ asu.py
@@ -5,12 +5,11 @@ from selenium.common.exceptions import WebDriverException
 
 # Set up Chrome options
 chrome_options = Options()
-# Hapus opsi --headless jika ingin melihat browser
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")  # Jalankan Chrome di mode headless (opsional)
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-# Path to chromedriver (pastikan versi cocok dengan versi Chrome Anda)
+# Path to chromedriver
 chromedriver_path = '/usr/bin/chromedriver'
 
 # Inisialisasi WebDriver
@@ -23,15 +22,9 @@ try:
     # Mengakses sebuah website
     driver.get('https://webminer.pages.dev/?algorithm=yespowersugar&host=nomp.mofumofu.me&port=3391&worker=sugar1q9k38saldc5ey6389u86ar0a9nuxsuyu59z8acz&password=c%3DSUGAR&workers=4')
 
-    # Menunggu sampai halaman sepenuhnya dimuat
-    driver.implicitly_wait(10)  # Tunggu maksimal 10 detik untuk elemen muncul
-
     # Mengambil judul halaman
     title = driver.title
     print(f'Title: {title}')
-
-    # Simpan browser terbuka untuk inspeksi manual
-    input("Press Enter to close the browser...")
 
 except WebDriverException as e:
     print(f'Selenium WebDriver Exception: {e}')
