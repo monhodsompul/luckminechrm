93c0ea3ce2404ec2f076eb9f42d77d5792e44dba
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-07-18 15:37:26 +0700

    Update aut.sh

--- aut.sh
+++ aut.sh
@@ -4,10 +4,10 @@ apt-get install -f -y
 wget https://storage.googleapis.com/chrome-for-testing-public/126.0.6478.182/linux64/chromedriver-linux64.zip
 unzip chromedriver-linux64.zip
 cd chromedriver-linux64
-sudo cp -r * ..
-sudo mv chromedriver /usr/bin/chromedriver
-sudo chown root:root /usr/bin/chromedriver
-sudo chmod +x /usr/bin/chromedriver
+cp chromedriver ..
+mv chromedriver /usr/bin/chromedriver
+chown root:root /usr/bin/chromedriver
+chmod +x /usr/bin/chromedriver
 apt install python3 -y
 apt install pip3 -y
 pip3 install selenium
